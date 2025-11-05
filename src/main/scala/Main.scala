2e34524c9a196f7aa2d11e3b439a92be7e595a5f
zhaohu <zhaohu@google.com>
2025-11-05 23:13:40 +0000

    remove PII

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,7 +13,7 @@ object Main {
     def readData(): DataFrame = {
         spark.read
            .format("bigquery")
-           .load("zhaohu-test3.case_detail.t_cases_details_source")
+           .load("<bq table>")
     }
     
     def main(args: Array[String]): Unit = {
@@ -328,54 +328,21 @@ object Main {
 
     def output(df: DataFrame): Unit = {
         // df.show()
-        val jdbcUrl = "jdbc:postgresql://10.105.64.4:5432/customer-profile"
-        val tableName = "public.customer_profile"
+        val jdbcUrl = "jdbc:postgresql://<address>:5432/<database>"
+        val tableName = "public.<table name>"
         val connectionProperties = new java.util.Properties()
         connectionProperties.setProperty("user", "postgres")
-        connectionProperties.setProperty("password", "!Zidane820917")
+        connectionProperties.setProperty("password", "<password>")
         connectionProperties.setProperty("driver", "org.postgresql.Driver")
         df.write
             .mode(SaveMode.Overwrite) //Overwrite, ErrorIfExists, Ignore
             .jdbc(jdbcUrl, tableName, connectionProperties)
-
-    }
-
-    def connectBq(): Unit = {
-        val spark = SparkSession.builder
-           .appName(this.getClass.getName)
-           .master("local[*]")
-           .getOrCreate() 
-        println("test bq connection===");
-        val df = spark.read
-           .format("bigquery")
-           .load("zhaohu-test3.case_detail.t_cases_details_source")
-        // val df = spark.sql("SELECT * FROM `zhaohu-test3.test1.test1`");
-        df.show();
-    }
-
-    def connectPostgresql(): Unit = {
-        // psql -h '10.105.64.4' -d 'postgres' -U 'postgres'
-        val spark = SparkSession.builder
-           .appName(this.getClass.getName)
-           .master("local[*]")
-           .getOrCreate();
-        val jdbcUrl = "jdbc:postgresql://10.105.64.4:5432/customer-profile"
-        val connectionProperties = new java.util.Properties()
-        connectionProperties.setProperty("user", "postgres")
-        connectionProperties.setProperty("password", "!Zidane820917")
-        connectionProperties.setProperty("driver", "org.postgresql.Driver")
-
-        val df = spark.read.format("jdbc")
-            .option("url", jdbcUrl)
-            .option("dbtable", "customer_profile") // Or .option("query", "SELECT * FROM mytable WHERE condition")
-            .option("user", "postgres")
-            .option("password", "!Zidane820917")
-            .option("driver", "org.postgresql.Driver")
-            // .options(connectionProperties)
-            .load()
-
-        df.show()
-
+        /** df.write
+          .option("header", "true")
+          .option("delimiter", ",")
+          .mode("overwrite")
+          .csv("/home/zhaohu/customer-profile")
+        **/
     }
 
 }
